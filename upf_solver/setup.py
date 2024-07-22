8d84ac9afc1286eb64c3d9b993b0afed297af2ce
Samuele Sandrini <samuele.sandrini@polito.it>
2024-07-22 18:41:21 +0200

    Test

--- upf_solver/setup.py
+++ upf_solver/setup.py
@@ -1,5 +1,6 @@
-import os
 from glob import glob
+import os
+
 from setuptools import find_packages, setup
 
 package_name = 'upf_solver'
--- upf_solver/upf_solver/upf_solver.py
+++ upf_solver/upf_solver/upf_solver.py
@@ -1,8 +1,9 @@
 import rclpy
 from rclpy.node import Node
 
-from unified_planning.shortcuts import *
 from unified_planning.io import PDDLReader, PDDLWriter
+from unified_planning.shortcuts import OneshotPlanner # OptimalityGuarantee
+
 
 class UpfSolver(Node):
     def __init__(self):
@@ -24,28 +25,28 @@ class UpfSolver(Node):
         self.get_logger().info(f'Output plan: {self.output_plan_path}')
 
     def load_problem(self):
-      reader = PDDLReader()
-      try:
-          self.parsed_problem = reader.parse_problem(self.domain_path, self.problem_path)
-      except Exception as e:
-          self.get_logger().error(f'Error loading problem: {e}')
+        reader = PDDLReader()
+        try:
+            self.parsed_problem = reader.parse_problem(self.domain_path, self.problem_path)
+        except Exception as e:
+            self.get_logger().error(f'Error loading problem: {e}')
 
     def solve(self):
-      with OneshotPlanner(name=self.solver) as planner: # optimality_guarantee=OptimalityGuarantee.SOLVED_OPTIMALLY
-        result = planner.solve(self.parsed_problem)
-        print(result.status)
-        print(result.plan)
-        writer = PDDLWriter(self.parsed_problem)
-        writer.write_plan(result.plan, self.output_plan_path)
+        with OneshotPlanner(name=self.solver) as planner: # optimality_guarantee=OptimalityGuarantee.SOLVED_OPTIMALLY
+            result = planner.solve(self.parsed_problem)
+            print(result.status)
+            print(result.plan)
+            writer = PDDLWriter(self.parsed_problem)
+            writer.write_plan(result.plan, self.output_plan_path)
 
 def main(args=None):
     rclpy.init(args=args)
     node = UpfSolver()
     try:
-      node.load_problem()
-      node.solve()
+        node.load_problem()
+        node.solve()
     except Exception as e:
-      node.get_logger().error(f'Error solving problem: {e}')  
+        node.get_logger().error(f'Error solving problem: {e}')  
     node.destroy_node()
     rclpy.shutdown()
 
