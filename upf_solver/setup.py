89e98ba5b6c36b175042cf6356449e9a650aa79d
Samuele Sandrini <samuele.sandrini@polito.it>
2024-07-23 15:31:14 +0200

    update

--- upf_solver/setup.py
+++ upf_solver/setup.py
@@ -5,6 +5,7 @@ from setuptools import find_packages, setup
 
 package_name = 'upf_solver'
 
+
 setup(
     name=package_name,
     version='0.0.0',
@@ -17,7 +18,8 @@ setup(
         (os.path.join('share', package_name, 'example'), glob('example/*.pddl')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools'
+                      'unified-planning[tamer]'],
     zip_safe=True,
     maintainer='kalman',
     maintainer_email='samuele.sandrini@polito.it',
